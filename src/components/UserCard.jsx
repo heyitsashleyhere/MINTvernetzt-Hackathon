b2b4f11047e7f5206d8576f338cbe5122fd724ba
Ashley Jiang <ashleyjiang4@gmail.com>
2022-10-15 18:37:44 +0200

    card can flip

--- src/components/UserCard.jsx
+++ src/components/UserCard.jsx
@@ -1,10 +1,13 @@
 import React, {useState} from 'react'
-import { Stack, Avatar, Card, CardContent, CardHeader, Typography } from "@mui/material";
+import { Stack, Avatar, Card, CardContent, CardHeader, Typography, Button, Divider } from "@mui/material";
 import ReactBoxFlip from 'react-box-flip';
+import MailOutlineIcon from '@mui/icons-material/MailOutline';
+import PhoneIcon from '@mui/icons-material/Phone';
+import LanguageIcon from '@mui/icons-material/Language';
 
-function FrontCard({user}) {
+function FrontCard({user, handleClick}) {
   return (
-    <Card sx={{ maxWidth: 345 }}>
+    <Card sx={{ maxWidth: 345 }} onClick={handleClick}>
       <CardHeader 
         avatar={ <Avatar src={user.photo}/> }
         title={`${user.firstName} ${user.lastName}`}
@@ -26,26 +29,21 @@ function FrontCard({user}) {
   )
 } 
 
-function BackCard({user}) {
+function BackCard({user, handleClick}) {
+  const date = (item) => new Date(item).toLocaleDateString("eu");
   return (
-    <Card sx={{ maxWidth: 345 }}>
-      <CardHeader 
-        avatar={ <Avatar src={user.photo}/> }
-        title={`${user.firstName} ${user.lastName}`}
-        subheader={user.position ? user.position : null}
-      />
-      
+    <Card sx={{ maxWidth: 345 }} onClick={handleClick}>
       <CardContent>
-        {user.description && (
-            <Typography variant="body2" sx={{mb: 2}}>{user.description}</Typography>
-        )}
-        {user.activityAreas && (
-            <Stack direction="row" spacing={2}>
-                <Typography>Activity area</Typography>
-                <Typography>{user.activityAreas.join('/')}</Typography>
-            </Stack>
-        )}
+          <img src={user.photo} style={{width: '20%'}} alt={`${user.firstName} ${user.lastName}`} />
+          <Typography color={'primary'}>{`${user.firstName} ${user.lastName}`}</Typography>
+          {user.position && <Typography>{user.position}</Typography>}
+          {user.email && <Button startIcon={<MailOutlineIcon />}>{user.email}</Button>}
+          {user.phone && <Button startIcon={<PhoneIcon />}>{user.phone}</Button>}
+          {user.website && <Button startIcon={<LanguageIcon />} />}
+        <Divider sx={{my: 1}} />
+        <Typography variant="caption" sx={{ display: 'flex', flexDirection: 'column',justifyContent: 'center', alignItems: 'center'}}>Profil besteht seit dem {date(user.dateCreated)}</Typography>
       </CardContent>
+      
     </Card>
   )
 } 
@@ -58,11 +56,10 @@ export default function UserCard({user}) {
 
   return (
         <>
-        <button onClick={handleClick}> Flip </button>
-            <ReactBoxFlip isFlipped={isFlipped}>
-                <FrontCard user={user} />
-                <BackCard user={user} />
-            </ReactBoxFlip>
+          <ReactBoxFlip isFlipped={isFlipped}>
+              <FrontCard user={user} handleClick={handleClick}/>
+              <BackCard user={user} handleClick={handleClick}/>
+          </ReactBoxFlip>
         </>
   )
 }
